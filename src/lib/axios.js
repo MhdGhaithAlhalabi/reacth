c3667dcbd93f53a63c401e4ce9546e669ade92bc
ghaithAlhalabi <78612789+ghaithAlhalabi@users.noreply.github.com>
2022-05-10 00:35:34 +0300

    e

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -4,12 +4,6 @@ const axios = Axios.create({
   baseURL: process.env.REACT_APP_BACKEND_URL,
   headers: {
     'X-Requested-With': 'XMLHttpRequest',
-    'Accept':'application/json',
-    'Access-Control-Allow-Methods':'GET, POST, OPTIONS',
-    'Access-Control-Allow-Headers':'DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type',
-   'Content-Type': 'application/json',
-   'Access-Control-Allow-Origin':'*',
-   'Access-Control-Allow-Credentials':'true',
   },
   withCredentials: true
 })
